fbff3c73551b483dbf819bc0e8ae9053fc2c3d53
Kevin Huang <origamijr@sbcglobal.net>
2021-01-28 23:27:48 -0800

    added wol

---
+++ MarxBot/Modules/WoL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace MarxBot.Modules {
+    public class WoL : ModuleBase<SocketCommandContext> {
+        [Command("phone-home")]
+        public async Task WoLAsync() {
+            if (Context.User.Id == 236746009688932354) {
+                ("sudo etherwake -i eth0 1C:1B:0D:38:8E:D6").Bash();
+                await ReplyAsync($"Home base has been contacted.");
+            } else {
+                await ReplyAsync($"You are not E.T.");
+            }
+        }
+    }
+}
